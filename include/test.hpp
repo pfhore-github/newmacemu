b3d817297fbc7f966ad6849d801477730cc300d2
pfhore-github <ookawa.michiaki@gmail.com>
2023-06-20 13:07:13 +0900

    test.hpp

--- include/test.hpp
+++ include/test.hpp
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <utility>
 #include <vector>
+#include "mpfr.h"
 extern std::vector<uint8_t> RAM;
 void reset_fpu();
 std::pair<std::function<void()>, int> decode();
@@ -12,6 +13,21 @@ struct Prepare {
     Prepare() ;
 };
 namespace TEST {
+inline void SET_FP(int n, double v) {
+    mpfr_set_d(cpu.FP[n], v, MPFR_RNDN);
+}
+inline double GET_FP(int n) {
+    return mpfr_get_d(cpu.FP[n], MPFR_RNDN);
+}
+inline bool FP_TEST(int n, double vv, double eps = 1e-10) {
+    MPFR_DECL_INIT(tmp, 64);
+    MPFR_DECL_INIT(tmp2, 64);
+    mpfr_set(tmp, cpu.FP[n], MPFR_RNDN);
+    mpfr_set_d(tmp2, eps, MPFR_RNDN);
+    mpfr_sub_d(tmp, tmp, vv, MPFR_RNDN);
+    mpfr_abs(tmp, tmp, MPFR_RNDN);
+    return mpfr_less_p(tmp, tmp2);
+}   
 inline uint16_t GET_W(uint32_t addr) { return RAM[addr] << 8 | RAM[addr + 1]; }
 inline uint32_t GET_L(uint32_t addr) {
     return RAM[addr] << 24 | RAM[addr + 1] << 16 | RAM[addr + 2] << 8 |
@@ -28,9 +44,16 @@ inline void SET_L(uint32_t addr, uint32_t v) {
     RAM[addr + 3] = v;
 }
 } // namespace TEST
+extern const double sg_v[2];
+extern const mpfr_rnd_t RND_MODES[4];
+
 inline int decode_and_run() {
     auto [f, i] = decode();
     f();
     return i;
 }
+
+void qnan_test(uint16_t op) ;
+void snan_test(uint16_t op) ;
+
 #endif
